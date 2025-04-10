29a0feb75f5b37f6b64944e61b05bfd42c615618
kiran278 <kirangowdag8@gmail.com>
2025-04-10 18:34:11 +0530

    second commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,48 @@
+$(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 15,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: true,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        425: {
+            items: 2,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+        }
+    }
+});
+
+
+AOS.init({
+    duration: 1000, // optional: animation duration in ms
+    once: true      // ✅ ensures the animation happens only once
+});
+
+// Scroll To Top
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 350) {
+        scrollToTopBtn.classList.remove('hidden');
+    } else {
+        scrollToTopBtn.classList.add('hidden');
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,js}" ],
+  theme: {
+    extend: {
+      fontFamily: {
+        laila: [ 'Laila', 'sans-serif' ],
+      },
+    },
+  },
+  plugins: [],
+}
+
